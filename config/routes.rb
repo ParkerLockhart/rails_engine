8f18cd7d59e87516b85a81d363e495e8724ba170
Parker Lockhart <parkerlockhart42@gmail.comm>
2022-02-08 21:30:46 -0600

    add items index, show routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,9 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :merchants, only: [:index, :show] do
-        resources :items, only: [:index]
-      end 
+        get '/items', to: 'merchant_items#index'
+      end
+      resources :items, only: [:index, :show]
     end
   end
 end
