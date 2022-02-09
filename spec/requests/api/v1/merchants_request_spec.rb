9ca709e3ad27bb92d2324c791e4ac35a2fc426ad
Parker Lockhart <parkerlockhart42@gmail.comm>
2022-02-08 21:30:18 -0600

    merchant test refactor

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -78,27 +78,29 @@ describe 'Merchants API endpoints' do
 
       expect(items[:data].count).to eq(3)
 
-      expect(items[:data].first).to have_key(:id)
-      expect(items[:data].first[:id]).to be_a(String)
+      items[:data].each do |item|
+        expect(item).to have_key(:id)
+        expect(item[:id]).to be_a(String)
 
-      expect(items[:data].first).to have_key(:type)
-      expect(items[:data].first[:type]).to eq('item')
+        expect(item).to have_key(:type)
+        expect(item[:type]).to eq('item')
 
-      expect(items[:data].first).to have_key(:attributes)
-      expect(items[:data].first[:attributes]).to be_a(Hash)
+        expect(item).to have_key(:attributes)
+        expect(item[:attributes]).to be_a(Hash)
 
-      attributes = items[:data].first[:attributes]
-      expect(attributes).to have_key(:name)
-      expect(attributes[:name]).to be_a(String)
+        attributes = item[:attributes]
+        expect(attributes).to have_key(:name)
+        expect(attributes[:name]).to be_a(String)
 
-      expect(attributes).to have_key(:description)
-      expect(attributes[:description]).to be_a(String)
+        expect(attributes).to have_key(:description)
+        expect(attributes[:description]).to be_a(String)
 
-      expect(attributes).to have_key(:unit_price)
-      expect(attributes[:unit_price]).to be_a(Float)
+        expect(attributes).to have_key(:unit_price)
+        expect(attributes[:unit_price]).to be_a(Float)
 
-      expect(attributes).to have_key(:merchant_id)
-      expect(attributes[:merchant_id]).to be_an(Integer)
+        expect(attributes).to have_key(:merchant_id)
+        expect(attributes[:merchant_id]).to be_an(Integer)
+      end
     end
   end
 end
