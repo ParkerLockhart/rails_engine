25129bc11526e9e89fadf857c23d31c51aad48be
Parker Lockhart <parkerlockhart42@gmail.comm>
2022-02-10 15:46:07 -0600

    Test for merchant search method

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -9,4 +9,20 @@ RSpec.describe Merchant, type: :model do
   describe 'validations' do
     it { should validate_presence_of(:name) }
   end
-end 
+
+  describe 'class methods' do
+    describe 'name_search_single' do
+      it 'returns first merchant matching search term' do
+        merchant_1 = create(:merchant, name: "Observe")
+        merchant_2 = create(:merchant, name: "Preserve")
+        merchant_3 = create(:merchant, name: "Deserve")
+        merchant_4 = create(:merchant, name: "Conserve")
+
+        expect(Merchant.name_search_single("serve")).to eq(merchant_4)
+        expect(Merchant.name_search_single("pre")).to eq(merchant_2)
+        expect(Merchant.name_search_single("des")).to eq(merchant_3)
+      end
+    end
+  end
+
+end
