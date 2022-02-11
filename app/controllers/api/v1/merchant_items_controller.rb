e07db8df72e070dca680ebcbdccca90fc148c0de
Parker Lockhart <parkerlockhart42@gmail.comm>
2022-02-11 09:00:10 -0600

    update error

--- app/controllers/api/v1/merchant_items_controller.rb
+++ app/controllers/api/v1/merchant_items_controller.rb
@@ -8,6 +8,6 @@ rescue_from ActiveRecord::RecordNotFound, with: :not_found
   end
 
   def not_found
-    respond_with '{"error": "not_found"}', status: :not_found
+    render json: "Error", status: :not_found
   end
 end
